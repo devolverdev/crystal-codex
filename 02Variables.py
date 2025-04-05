2f3cdbf4004ea97528b372fb4c6f060dfeaea066
Bruna School <brunaschool@Brunas-MacBook-Pro.local>
2025-04-05 18:29:32 +0200

    📦 Add variable intro with mood-based selections

---
+++ 02Variables.py
@@ -0,0 +1,56 @@
+## Example 01 - Basic Variables
+
+sky = "melting"
+print("Sky is:", sky)
+
+
+## Example 2 - Conditional based on Logic
+
+sunny = False
+if sunny:
+    print("Turn off the lights")
+else:
+    print("Keep lights on")
+
+## Example 3 - Looping
+
+for i in range(3):
+    print(i + 1, "Misissipi")
+
+   
+
+ 
+###
+
+ for step in range(4):
+    print("Step number:", step)
+
+
+###
+
+ print(list(range(5)))
+
+
+### Example 4 - Logic and Loop
+
+is_raining = False
+
+for i in range(4):
+    if is_raining:
+        print("🌧️ Delay watering", i + 1)
+    else:
+        print("💧 Water plant", i + 1)
+
+
+### Tiny sun
+
+sunny = True
+
+if sunny:
+    print(" \\   / ")
+    print("  ☀️   ")
+    print(" /   \\")
+else:
+    print("It's cloudy.")
+
+
